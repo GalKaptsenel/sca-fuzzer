6f4d1f9184c8c78eb3ec68546f34acc55afb0021
Gal Kaptsenel <gal.k@campus.technion.ac.il>
2025-11-25 19:22:51 +0200

    added div and conbitional-selects aarch64 instructions

--- src/aarch64/aarch64_config.py
+++ src/aarch64/aarch64_config.py
@@ -190,7 +190,11 @@ _buggy_instructions: List[str] = [
     # "maskmovdqu",  # incorrect emulation
 ]
 
-supported_instructions: List[str] = ["adds", "subs", "b.", "cbz", "b", "str", "ldr", "orr", "ands", "eor", "cbnz"]
+supported_instructions: List[str] = ["adds", "subs", "b.", "cbz", "b", "str", "ldr", "orr", "ands", "eor", "cbnz",
+                                     "csel", "csinc", "csinv", "csneg", "ccmn", "ccmp",
+                                     "sdiv", "udiv"
+                                     ]
+#                                     "ldp", "stp"]
 
 instruction_blocklist: List[str] = [
     # Currently don't support them - they require very specific order of instruction (they must appear one after the other, what happens otherwise? I don't know)
